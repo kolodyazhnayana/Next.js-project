0e6a79e268fef1806bc0c2fa184a3489c5e6d211
kolodyazhnayana <kolodyazhnaya19@yandex.ru>
2021-07-22 13:16:36 +0300

    add title on posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,13 @@
-import Link from 'next/link'
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import Head from 'next/head'
 
 const FirstPost = () => {
     return (
         <Layout>
+            <Head>
+                <title>Posts</title>
+            </Head>
             Posts
         </Layout>
     )
